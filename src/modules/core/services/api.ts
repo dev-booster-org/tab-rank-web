fa7fb40d801347ccf202425e7911f157d497f7e7
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2025-11-22 11:35:21 -0300

    feat: implement interceptor to logout unauthorized users

--- src/modules/core/services/api.ts
+++ src/modules/core/services/api.ts
@@ -5,3 +5,18 @@ console.log('API URL:', import.meta.env.VITE_API_URL)
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + '/api',
 })
+
+api.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('tabRank:accessToken')
+      localStorage.removeItem('tabRank:user')
+
+      window.location.href = '/'
+    }
+
+    return Promise.reject(error)
+  },
+)
